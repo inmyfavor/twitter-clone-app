fb42702cbe4ec09f0df7f307755cbe7b902a61a9
inmyfavor <coolkidoriginal@icloud.com>
2023-01-20 22:33:50 +0300

    .

--- src/user/User.jsx
+++ src/user/User.jsx
@@ -2,18 +2,47 @@ import React from 'react';
 import Profile from './profile/Profile';
 import UserAvatar from './UserAvatar';
 
+const ReplyCategory = (props) => {
+    return (
+        <div className='flex flex-row gap-[10px] items-center'>
+            <div className='h-[40px] w-[40px] rounded-[20px] bg-blue'></div>
+            <div className='font-semibold text-[14px]'>{props.title}</div>
+        </div>
+    );
+};
+
+const WhoCanReply = () => {
+    return (
+        <div className='h-[300px] w-[350px] flex flex-col bg-white shadow-[0_0_5px_1px_rgba(0,0,0,0.1)] rounded-[8px] p-[15px]'>
+            <div className='text-[16px] font-semibold mb-[5px]'>Кто может отвечать?</div>
+            <div className='text-[14px] text-gray mb-[20px]'>
+                Выберите, кто может отвечать на этот твит. Упомянутым пользователям эта возможность доступна всегда.
+            </div>
+            <div className='flex flex-col gap-[15px]'>
+                <ReplyCategory title='Все'/>
+                <ReplyCategory title='Люди, которых вы читаете'/>
+                <ReplyCategory title='Только упомянутые вами пользователи'/>
+            </div>
+        </div>
+    );
+};
+
 const CreateTweet = () => {
     return (
         <div className='flex flex-row gap-[15px] p-[15px] border-b-[2px] border-light-gray'>
             <UserAvatar/>
-            <div className='w-full flex flex-col'>
+            <div className='flex flex-col'>
                 <div className='relative h-[24px] w-[60px] mb-[20px]'>
                     <button className='w-full h-full border border-button-gray text-[14px] pr-[20px] rounded-[10px] text-blue hover:bg-blue hover:bg-opacity-[0.1]'>
                         Все
                     </button>
                     <img alt='' src='svg/forwardarrow.svg' className='absolute top-[6px] right-[8px] w-[16px] h-[16px] rotate-90 pointer-events-none'/>
                 </div>
-                
+                <div className='text-[18px] text-gray mb-[10px]'>Что происходит?</div>
+                <div className='ml-[-10px] hover:bg-blue hover:bg-opacity-[0.1] rounded-[8px] h-[24px]'>
+                    <div className='text-blue text-[14px] px-[10px]'>Отвечать могут все пользователи</div>
+                </div>
+                <WhoCanReply/>
             </div>
         </div>
     );
